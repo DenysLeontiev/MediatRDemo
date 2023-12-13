eab3f2c6441b83f4c87e8142401da805d25292b8
Denys Leon <leonden0046@gmail.com>
2023-12-14 00:04:16 +0200

    Added InsertPerson Command/Handler

---
+++ Persistance/Features/Person/Commands/InsertPersonModel/InsertPersonCommad.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Persistance.Models;
+
+namespace Persistance.Features.Person.Commands.InsertPersonModel;
+
+public record InsertPersonCommad(string FirstName, string LastName) : IRequest<PersonModel>;
---
+++ Persistance/Features/Person/Commands/InsertPersonModel/InsertPersonHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Persistance.DataAccess;
+using Persistance.Models;
+
+namespace Persistance.Features.Person.Commands.InsertPersonModel;
+
+public class InsertPersonHandler : IRequestHandler<InsertPersonCommad, PersonModel>
+{
+	private readonly IDataAccess _dataAccess;
+	public InsertPersonHandler(IDataAccess dataAccess)
+	{
+		_dataAccess = dataAccess;
+	}
+
+	public async Task<PersonModel> Handle(InsertPersonCommad request, CancellationToken cancellationToken)
+	{
+		var createdPerson = _dataAccess.InsertPersonModel(request.FirstName, request.LastName);
+
+		return createdPerson;
+	}
+}
