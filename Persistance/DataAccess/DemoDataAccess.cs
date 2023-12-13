2aa32949cb608533608a441ea6f5efddc4882197
Denys Leon <leonden0046@gmail.com>
2023-12-13 21:49:49 +0200

    Added PersonModel and DataAccess

---
+++ Persistance/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Persistance.Models;
+
+namespace Persistance.DataAccess;
+
+public class DemoDataAccess : IDataAccess
+{
+	private List<PersonModel> people = new List<PersonModel>();
+
+	public DemoDataAccess()
+	{
+		people.Add(new PersonModel { Id = 1, FirstName = "Den", LastName = "Leon" });
+		people.Add(new PersonModel { Id = 2, FirstName = "Tim", LastName = "Correy" });
+	}
+
+	public List<PersonModel> GetPeople()
+	{
+		return people;
+	}
+
+	public PersonModel InsertPersonModel(string firstName, string lastName)
+	{
+		PersonModel personModel = new() { FirstName = firstName, LastName = lastName };
+		personModel.Id = people.Max(x => x.Id) + 1;
+
+		people.Add(personModel);
+
+		return personModel;
+	}
+}
---
+++ Persistance/DataAccess/IDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using Persistance.Models;
+
+namespace Persistance.DataAccess;
+
+public interface IDataAccess
+{
+	List<PersonModel> GetPeople();
+	PersonModel InsertPersonModel(string firstName, string lastName);
+}
